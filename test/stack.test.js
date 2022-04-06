f11e2426d8b03a1e8323168747eb16db994a6ae2
Sarah Mottram <smelbows@gmail.com>
2022-04-06 11:37:14 +0200

    refactor test suite to avoid repetition of new Stack

--- test/stack.test.js
+++ test/stack.test.js
@@ -6,10 +6,15 @@ class Stack {
 }
 
 describe('My Stack', () => {
-  it('is created empty', () => {
-    const stack = new Stack();
+  let stack;
 
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  it('is created empty', () => {
     expect(stack.top).toBe(-1);
+    expect(stack.items).toEqual({});
   });
 
   it.todo('can push to the top');
