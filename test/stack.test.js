ee7f691661d4bfdc68cd43f6a2b40ea22b014a3a
Sarah Mottram <smelbows@gmail.com>
2022-04-13 09:06:06 +0200

    added tests and updated class

--- test/stack.test.js
+++ test/stack.test.js
@@ -3,6 +3,21 @@ class Stack {
     this.top = -1;
     this.items = {};
   }
+
+  get peak() {
+    return this.items[this.top];
+  }
+
+  push(value) {
+    this.top += 1;
+    this.items[this.top] = value;
+  }
+
+  pop() {
+      delete this.items[this.top]
+      this.top -= 1;
+
+  }
 }
 
 describe('My Stack', () => {
@@ -17,7 +32,16 @@ describe('My Stack', () => {
     expect(stack.items).toEqual({});
   });
 
-  it.todo('can push to the top');
+  it('can push to the top', () => {
+    stack.push('🥑');
+    expect(stack.top).toBe(0);
+    expect(stack.peak).toBe('🥑');
+  });
 
-  it.todo('can pop off');
+  it('can pop off', () => {
+    stack.push('🥑');
+    stack.pop();
+    expect(stack.top).toBe(-1);
+    expect(stack.items).toEqual({});
+  });
 });
